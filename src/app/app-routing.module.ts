cd71d55fd645b9fb971a1a2da41ffaffb5cf882b
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2022-08-25 16:33:55 -0300

    Ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Componentes
+import { ListadoMascotaComponent } from './components/listado-mascota/listado-mascota.component';
+import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
+import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'listMascotas', pathMatch: 'full' },
+  { path:'listMascotas', component: ListadoMascotaComponent },
+  { path:'agregarMascota', component: AgregarEditarMascotaComponent },
+  { path:'verMascota/:id', component: VerMascotaComponent },
+  { path:'editarMascota/:id', component: AgregarEditarMascotaComponent },
+  { path: '**',  redirectTo: 'listMascotas', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
